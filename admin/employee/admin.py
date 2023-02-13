f39a214afa453c41e9dfe9bb9136373e45e7b359
anointedngeorge <anointedngeorge@gmail.com>
2023-02-13 13:56:57 +0100

    cw4343sd

--- admin/employee/admin.py
+++ admin/employee/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-# from employee.models import *
-# # Register your models here.
+from employee.models import *
+# Register your models here.
 
 
-# @admin.register(Employee)
-# class employeeAdmin(admin.ModelAdmin):
-#     list_display = ['user']
-#     exclude = ['user']
+@admin.register(Employee)
+class employeeAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    exclude = ['user']
 
-#     fieldsets = (
-#       ('Personal Information', {
-#           'fields': ('address','phone_number','gender','marital_status',)
-#       }),
+    fieldsets = (
+      ('Personal Information', {
+          'fields': ('address','phone_number','gender','marital_status',)
+      }),
       
-#       ('Employment History', {
-#           'fields': ('designation','title','employment_type','department','skills','salary',)
-#       }),
+      ('Employment History', {
+          'fields': ('designation','title','employment_type','department','skills','salary',)
+      }),
       
-#       ('Location History', {
-#           'fields': ('country','state','local_government','town',)
-#       }),
+      ('Location History', {
+          'fields': ('country','state','local_government','town',)
+      }),
       
-#       ('Short Description', {
-#           'fields': ('about',)
-#       }),
-#    )
+      ('Short Description', {
+          'fields': ('about',)
+      }),
+   )
 
-#     def has_add_permission(self, request) -> bool:
-#         return False
+    def has_add_permission(self, request) -> bool:
+        return False
