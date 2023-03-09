bf081da48c4546d284a402455671fee86db63cee
anointedngeorge <anointedngeorge@gmail.com>
2023-03-09 10:29:13 +0100

    cewesddd

--- admin/plugins/dropdown.py
+++ admin/plugins/dropdown.py
@@ -27,56 +27,62 @@ def dictDropdown(action=[], link='', status='', modelname='', code='', report_ti
         This is a dropdown menue
         """
         html = ""
-        # get the status that match in the action object
-        get_status =  None
-        if not action.get(status) == None:
-            get_status =  action.get(status)
-        else:
-            get_status = []
-            
-        html += "<div class='table table-responsive'>"
-        html += "<table class='table table-sm table-condensed'>"
-        html += "<tr>"
-        query = ''
-        for x in get_status:
-            query = queryFormat(x.get('query')) if x.get('query') != None else ''
-            if x.get('is_button'):
-                html += f"<td><button type='button' data-url='{x.get('href')}' value='{query}'>{str(x.get('name')).title()}</button></td>"
+        try:
+            # get the status that match in the action object
+            get_status =  None
+            if not action.get(status) == None:
+                get_status =  action.get(status)
             else:
-                html += f"<td><a  href='{x.get('href')}?{query}'>{str(x.get('name')).title()}</a></td>"
-        html += f"<td><a  href='{local_file_url_image(code)}?model={modelname}'>Upload File(s)</a></td>"
-        html += f"<td><a  href='{api_fetch_image(code)}?model={modelname}' target='_blank'>Get Files</a></td>"
-        if is_report:
-            html += f"<td><a  href='{link}/{report_template_name}/{modelname}/?{query}'>{report_title.title()}</a></td>"
+                get_status = []
+                
+            html += "<div class='table table-responsive'>"
+            html += "<table class='table table-sm table-condensed'>"
+            html += "<tr>"
+            query = ''
+            for x in get_status:
+                query = queryFormat(x.get('query')) if x.get('query') != None else ''
+                if x.get('is_button'):
+                    html += f"<td><button type='button' data-url='{x.get('href')}' value='{query}'>{str(x.get('name')).title()}</button></td>"
+                else:
+                    html += f"<td><a  href='{x.get('href')}?{query}'>{str(x.get('name')).title()}</a></td>"
+            html += f"<td><a  href='{local_file_url_image(code)}?model={modelname}'>Upload File(s)</a></td>"
+            html += f"<td><a  href='{api_fetch_image(code)}?model={modelname}' target='_blank'>Get Files</a></td>"
+            if is_report:
+                html += f"<td><a  href='{link}/{report_template_name}/{modelname}/?{query}'>{report_title.title()}</a></td>"
 
-        html += "</tr>"
-        html += "</table>"
-        html += "</div>"
+            html += "</tr>"
+            html += "</table>"
+            html += "</div>"
 
-        return format_html(html)
+            return format_html(html)
+        except:
+            pass
 
 
 def singleDropdown(action=[], modelname='', link='', code='', report_title='', report_template_name='task', is_report=False):
-        html = ""
-        # get the status that match in the action object
-        html += "<div class='table table-responsive'>"
-        html += "<table class='table table-sm table-condensed'>"
-        html += "<tr>"
-        query = ''
-        for x in action:
-            query = queryFormat(x.get('query')) if x.get('query') != None else ''
-            if x.get('is_button'):
-                html += f"<td><button  data-url='{x.get('href')}' value='{query}'>{x.get('name')}</button></td>"
-            else:
-                html += f"<td><a href='{x.get('href')}?{query}'>{x.get('name')}</a></td>"
-        html += f"<td><a  href='{local_file_url_image(code)}?model={modelname}'>Upload File(s)</a></td>"
-        html += f"<td><a  href='{api_fetch_image(code)}?model={modelname}' target='_blank'>Get Files</a></td>"
-        
-        if is_report:
-            html += f"<td><a  href='{link}/{report_template_name}/{modelname}/?{query}'>{report_title.title()}</a></td>"
-        
-        html += "</tr>"
-        html += "</table>"
-        html += "</div>"
+        try:
+            html = ""
+            # get the status that match in the action object
+            html += "<div class='table table-responsive'>"
+            html += "<table class='table table-sm table-condensed'>"
+            html += "<tr>"
+            query = ''
+            for x in action:
+                query = queryFormat(x.get('query')) if x.get('query') != None else ''
+                if x.get('is_button'):
+                    html += f"<td><button  data-url='{x.get('href')}' value='{query}'>{x.get('name')}</button></td>"
+                else:
+                    html += f"<td><a href='{x.get('href')}?{query}'>{x.get('name')}</a></td>"
+            html += f"<td><a  href='{local_file_url_image(code)}?model={modelname}'>Upload File(s)</a></td>"
+            html += f"<td><a  href='{api_fetch_image(code)}?model={modelname}' target='_blank'>Get Files</a></td>"
+            
+            if is_report:
+                html += f"<td><a  href='{link}/{report_template_name}/{modelname}/?{query}'>{report_title.title()}</a></td>"
+            
+            html += "</tr>"
+            html += "</table>"
+            html += "</div>"
 
-        return format_html(html)
+            return format_html(html)
+        except:
+            pass
