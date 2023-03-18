7f8de6382033064ccc231f2cf8ea86ba1b7d0997
anointedngeorge <anointedngeorge@gmail.com>
2023-03-18 16:42:26 +0100

    c3dsdghhffd

--- admin/human_resource/models.py
+++ admin/human_resource/models.py
@@ -5,6 +5,6 @@ from human_resource.submodels.jobs import *
 from human_resource.submodels.jobs import (Job_history, Jobs)
 from human_resource.submodels.salary import *
 from human_resource.submodels.advert import *
-from human_resource.submodels.task import *
+# from human_resource.submodels.task import *
 from human_resource.submodels.model_appointment import *
 from human_resource.submodels.drawing_bank import *
